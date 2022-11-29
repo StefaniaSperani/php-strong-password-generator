bf8d237c0a111b0c36f223a1be6884a4a0ae06b7
StefaniaSperani <thedarkcry@hotmail.it>
2022-11-29 14:48:04 +0100

    add form + style

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<!-- Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato 
+per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php 
+che includeremo poi nella pagina principale
+
+Milestone 3
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata 
+che tramite $_SESSION recupererà la password da mostrare all’utente.
+
+Milestone 4 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. 
+Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
+
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +42,24 @@
 
 <body>
     <main>
+        <div class="container">
+            <h1 class="text-uppercase text-center fw-bold">Password generator</h1>
+            <form action="index.php" method="GET">
+                <div class="row g-3 align-items-center justify-content-center">
+                    <div class="col-auto">
+                        <label for="inputPassword6" class="col-form-label text-capitalize">Lunghezza Password</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="password" id="inputPassword6" class="form-control" required>
+                    </div>
+                </div>
+                <div class="d-flex p-3 align-items-center justify-content-center">
+                    <button type="submit" class="btn btn-primary">Invia</button>
+                    <button type="reset" class="btn btn-secondary">Annulla</button>
+                </div>
 
+            </form>
+        </div>
     </main>
 </body>
 
